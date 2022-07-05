a0e24c8c23d3916390f902b3540f9da7df45e4ac
Karthik-Gupta <karthikts.gupta@gmail.com>
2022-07-05 20:15:29 +0530

    Turing name update

--- cypress/integration/risk.test.ts
+++ cypress/integration/risk.test.ts
@@ -5,10 +5,10 @@ context("Risk Flow", () => {
 
   it("renders the Risk view and its contents", () => {
     cy.get('[data-testid="risk-intro"]').contains(
-      "Providing liquidity to Saddle",
+      "Providing liquidity to Turing",
     )
-    cy.get('[data-testid="risk-audits"]').contains("The Saddle smart")
-    cy.get('[data-testid="risk-adminkeys"]').contains("Saddle's admin keys")
+    cy.get('[data-testid="risk-audits"]').contains("The Turing smart")
+    cy.get('[data-testid="risk-adminkeys"]').contains("Turing's admin keys")
     cy.get('[data-testid="risk-lossofpeg"]').contains("If one of the assets")
   })
 })
